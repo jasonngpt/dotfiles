6b8bdd2a5791e446e01d88852b00a96a890e5bb6
Jason Ng PT <me@jasonngpt.com>
2013-12-25 11:55:21 +0800

    added update shell script to update git repos

---
+++ updaterepos.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Update Script for Git Repos
+# Jason Ng PT <me@jasonngpt.com>
+
+clear
+echo "Welcome to Update Script"
+echo
+
+echo "Updating Vundle from Github.."
+cd ~/.vim/bundle/vundle
+git pull origin
+
+echo
+echo "Updating oh-my-zsh.."
+cd ~/.oh-my-zsh
+git pull origin
+
+echo "Updating rbenv.."
+cd ~/.rbenv
+git pull origin
+echo "Updating ruby-build.."
+cd ~/.rbenv/plugins/ruby-build
+git pull origin
+
+echo "Updating plenv.."
+cd ~/.plenv
+git pull origin
+echo "Cloning perl-build.."
+cd ~/.plenv/plugins/perl-build
+git pull origin
+
+echo "Updating nvm.."
+cd ~/.nvm
+git pull origin
